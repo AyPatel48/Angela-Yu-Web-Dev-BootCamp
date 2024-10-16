d4c0d83dd664ff4945e2ba760a9238420945bc32
Patel <apatel574@myseneca.ca>
2024-10-16 02:19:59 -0400

    express done

--- Section 23/2.4 QR Code Project/index.js
+++ Section 23/2.4 QR Code Project/index.js
@@ -1,6 +1,4 @@
-/* 
-1. Use the inquirer npm package to get user input.
-*/
+
 import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
@@ -16,6 +14,10 @@ inquirer
     const URL = answers.URL;
     var qr_svg = qr.image(URL);
     qr_svg.pipe(fs.createWriteStream("qr.png"));
+    fs.writeFile("URL.txt", URL, (err) => {
+        if(err) throw err;
+        console.log("The file has been saved!");
+    })
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -24,16 +26,7 @@ inquirer
       // Something else went wrong
     }
   });
-
+ 
+//1. Use the inquirer npm package to get user input.
 // 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-// var qr = require("qr-image");
-
-// var qr_svg = qr.image(`${userURL}`, { type: "svg" });
-// qr_svg.pipe(require("fs").createWriteStream("qr.svg"));
 // // 3. Create a txt file to save the user input using the native fs node module.
-
-// const fs = require("fs");
-// fs.writeFile("message.txt", `${userURL}`, (err) => {
-//   if (err) throw err;
-//   console.log(userURL);
-// });
--- Section 24/3.4 Middleware/index4.js
+++ Section 24/3.4 Middleware/index4.js
@@ -1,8 +1,24 @@
 import express from "express";
 
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({extended: true}));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post("/submit", (req, res) => {
+  var bandName = req.body["street"] + req.body["pet"];
+
+  res.send(`<h1>Your band name is: </h1><br><p>${bandName}</p>`);
+})
