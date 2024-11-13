05ef03ccd6fd16581eb21b1d8b90c67703e0441b
Ayushkumar Patel <ayushpatel1622@gmail.com>
2024-11-12 20:14:08 -0500

    completed the simon game

--- Section 20/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Section 20/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,80 +1,108 @@
+//Author : Ayush Patel
+//Github : AyPatel48
+
+//Variable declaration
 var userClickedPattern = [];
 var gamePattern = [];
 var randomChosenColour = "";
 var buttonColours = ["red", "blue", "green", "yellow"];
 var level = 0;
+gameStarted = false;
 
-function nextSequence(){
-    $("#level-title").text("Level " + level);
-    var randomNumber = Math.floor(Math.random() * 4);
-    randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
+//Event listeners
 
-    //Use jQuery to select the button with the same id as the randomChosenColour
-    $("#"+randomChosenColour).fadeOut(500).fadeIn(500);
-    playSound(randomChosenColour);
-    level++;
-}
+    //1. When the player clicks on a button
+    $("[type='button']").on("click", onButtonClick);
 
-function playSound(randomChosenColour){
-    switch(randomChosenColour){
-        case "green":
-            var audio = new Audio("./sounds/green.mp3");
-            audio.autoplay = true;
-            audio.play();
-            break;
-
-        case "blue":
-            var audio = new Audio("./sounds/blue.mp3");
-            audio.autoplay = true;
-            audio.play();
-            break;
-
-        case "red":
-            var audio = new Audio("./sounds/red.mp3");
-            audio.autoplay = true;
-            audio.play();
-            break;
-        
-        case "yellow":
-            var audio = new Audio("./sounds/yellow.mp3");
-            audio.autoplay = true;
-            audio.play();
-            break;
-
-        default:
-            var audio = new Audio("sounds/wrong.mp3");
-            audio.autoplay = true;
-            audio.play();
+    //2. When a key is pressed indicating the start or restart of the game
+    $(document).keydown(function (event) {
+    // Check if the pressed key is not F5
+    if (event.key !== "F5") {
+        // Only start the game if it hasn't already started
+        if (!gameStarted) {
+        gameStarted = true;
+        nextSequence();
+        }
     }
+    });
+
+//Gets the random next sequence of the game
+function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  //Select the button with the same id as the randomChosenColour
+  $("#" + randomChosenColour)
+    .fadeOut(500)
+    .fadeIn(500);
+  playSound(randomChosenColour);
 }
 
-//When the player clicks on a button
-$("[type='button']").on("click", onButtonClick);
+//Plays sound corresponding to the colour of the button clicked or to the event when game ends.
+function playSound(randomChosenColour = "wrong") {
+  var audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
+  audio.autoplay = true;
+  audio.play();
+}
 
 //What happens when a button is clicked
-function onButtonClick(event){
-    var userChosenColour = $(event.target).attr("id").replace('#', '');
-    
+function onButtonClick(event) {
+  if (gameStarted) {
+    var userChosenColour = $(event.target).attr("id").replace("#", "");
+
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    checkPlayerInput(userClickedPattern.length - 1);
+  } else {
+    wrongInput();
+  }
 }
 
-function animatePress(currentColour){
-    $("#"+currentColour).addClass("pressed");
-    setTimeout(function(){
-        $("#"+currentColour).removeClass("pressed");
-    }, 100);
+//Check players input i.e., the button clicked by the player is correct as per the gamePattern sequence or not.
+function checkPlayerInput(index) {
+  //If yes, then...
+  if (userClickedPattern[index] === gamePattern[index]) {
+    if (index === gamePattern.length - 1) {
+      userClickedPattern = [];
+      nextSequence();
+    }
+  }
+  //If not, then...
+  else {
+    wrongInput();
+  }
 }
 
-function startGame(){
-    nextSequence();
-    
-    console.log(level);
+//Handles the game incase of a wrong player input i.e, not matching the gamePattern sequence
+function wrongInput() {
+  $("#level-title").text("Game Over, Press Any Key to Restart");
+  animatePress();
+  playSound();
+  startOver();
 }
 
-$(document).on("keypress", startGame);
-$(document).off("keypress");
+//Resets the variables indicating the restart of the game
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  gameStarted = false;
+}
 
-startGame();
+//Animates the buttons when clicked by the player or when
+function animatePress(currentColour = "body") {
+  var effect = "game-over";
+  var selector = currentColour;
+
+  if (currentColour != "body") {
+    selector = "#" + currentColour;
+    effect = "pressed";
+  }
+  $(selector).addClass(effect);
+  setTimeout(function () {
+    $(selector).removeClass(effect);
+  }, 100);
+}
