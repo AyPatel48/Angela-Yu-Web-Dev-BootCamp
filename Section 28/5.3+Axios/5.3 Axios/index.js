1a28ef010ebac7873cf2f75c44ab8a62fcf067ed
Ayushkumar Patel <ayushpatel1622@gmail.com>
2025-04-12 11:48:52 -0400

    api using axios

--- Section 28/5.3+Axios/5.3 Axios/index.js
+++ Section 28/5.3+Axios/5.3 Axios/index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,6 +35,18 @@ app.post("/", async (req, res) => {
   // Step 3: If you get a 404 error (resource not found) from the API request.
   // Pass an error to the index.ejs to tell the user:
   // "No activities that match your criteria."
+    try {
+      const type = req.body.type;
+      const participants = req.body.participants;
+      const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`);
+      const result = response.data;
+      console.log(result);
+      res.render("index.ejs", {data : result[Math.floor(Math.random() * result.length)]});
+    }catch(error){
+      console.log("Failed to make request: ", error.message);
+      res.render("index.ejs", {error : "No activities that match your criteria."});
+    }
+
 });
 
 app.listen(port, () => {
