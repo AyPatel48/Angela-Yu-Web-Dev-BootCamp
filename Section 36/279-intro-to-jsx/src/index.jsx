97373ce61c8aed494269a51435fde245ce19d940
Ayushkumar Patel <ayushpatel1622@gmail.com>
2025-04-26 10:41:21 -0400

    add files

---
+++ Section 36/279-intro-to-jsx/src/index.jsx
@@ -0,0 +1,11 @@
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+//ReactDOM.render(What to show, Where to show)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1>Hello World!</h1>);
