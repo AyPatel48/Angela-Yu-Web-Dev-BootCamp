521cb86f4b2a6938e1e7ac09729019e3bc06178e
Ayushkumar Patel <ayushpatel1622@gmail.com>
2025-04-26 22:23:23 -0400

    complete complex state challenge

--- Section 36/313-changing-complex-state-start/313-changing-complex-state/src/components/App.jsx
+++ Section 36/313-changing-complex-state-start/313-changing-complex-state/src/components/App.jsx
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    switch (name) {
+      case "fName":
+        setContact((prevValue) => {
+          (fName = value), (lName = prevValue.lName), (email = prevValue.email);
+        });
+      case "lName":
+        setContact((prevValue) => {
+          (fName = prevValue.fName), (lName = value), (email = prevValue.email);
+        });
+      case "email":
+        setContact((prevValue) => {
+          (fName = prevValue.fName), (lName = prevValue.lName), (email = value);
+        });
+      default:
+        setContact((prevValue) => {
+          (fName = prevValue.fName),
+            (lName = prevValue.lName),
+            (email = prevValue.email);
+        });
+    }
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p style={{ alignSelf: "center" }}>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input name="fName" placeholder="First Name" onChange={handleChange} />
+        <input name="lName" placeholder="Last Name" onChange={handleChange} />
+        <input
+          name="email"
+          placeholder="Enter your email"
+          onChange={handleChange}
+        />
         <button>Submit</button>
       </form>
     </div>
